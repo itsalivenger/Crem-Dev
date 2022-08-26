93125db3ba7564f9b92eb1f26903d0d5b184a9db
ali houssa <81270840+itsalivenger@users.noreply.github.com>
2022-08-26 18:27:01 +0100

    lightswitch phone display

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,17 @@
 let menu = document.querySelector('.toggle-Menu');
+let lightswitch = document.querySelector('.slider');
 let greg = false;
+let gregor = false;
 
 
 function toggle() {
     menu.className = greg ? 'toggle-Menu' : 'toggle-Menu toggle';
     greg = !greg;
 }
+
+function handleMode() {
+    gregor = !gregor;
+}
+
+
+lightswitch.onclick = handleMode;
