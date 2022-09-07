aab219f2a3be13f6c87fc33fe79799a47056206b
ali houssa <81270840+itsalivenger@users.noreply.github.com>
2022-09-07 21:27:12 +0100

    images and our services section

---
+++ scripts/contentToggling.js
@@ -0,0 +1,17 @@
+const services = document.querySelectorAll('.services');
+let toggler = false;
+
+for (let i = 0; i < services.length; i++) {
+    services[i].addEventListener('mouseover', ()=> toggleContent(services[i]));
+    services[i].addEventListener('mouseout', ()=> toggleContent(services[i]));
+}
+
+const toggleContent = (ele)=>{
+    const textInServices = ele.querySelector('.serviceContentTextUntoggled');
+    const bg = ele.querySelector('.backgrounds');
+    bg.style.filter = toggler ? "blur(0)" : "blur(8px)";
+    ele.querySelector('.serviceContent').className = toggler ? 'serviceContent' : 'serviceContent toggleContent' ;
+    textInServices.style.background = toggler ? '' : 'rgba(0, 0, 0, .5)';
+    textInServices.className = toggler ? 'serviceContentTextUntoggled' : 'serviceContentTextUntoggled serviceContentTextToggled'
+    toggler = !toggler;
+}
