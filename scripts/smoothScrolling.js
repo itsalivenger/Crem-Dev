5079b53063767dd68a6845e4b4a948f746f958f9
ali houssa <81270840+itsalivenger@users.noreply.github.com>
2022-09-17 02:35:06 +0100

    animation and gallery

--- scripts/smoothScrolling.js
+++ scripts/smoothScrolling.js
@@ -1,6 +1,7 @@
 const observer = new IntersectionObserver((entries)=>{
   // console.log(entries);
   entries.forEach((entry)=>{
+    console.log(entry)
     if(entry.isIntersecting){
       entry.target.classList.add('show');
     }else{
