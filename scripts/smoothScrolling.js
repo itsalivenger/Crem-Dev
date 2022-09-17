ae5b1bc96306a92391019eea16b8373ba88b5af1
ali houssa <81270840+itsalivenger@users.noreply.github.com>
2022-09-17 15:22:40 +0100

    animation on hover on element

--- scripts/smoothScrolling.js
+++ scripts/smoothScrolling.js
@@ -1,8 +1,15 @@
 const observer = new IntersectionObserver((entries)=>{
   // console.log(entries);
   entries.forEach((entry)=>{
-    console.log(entry)
     if(entry.isIntersecting){
+      // to start the animation just once for the cards in gallery
+      if(entry.target.className.includes('gallery')){
+        for (let i = 0; i < cards.length; i++) {
+          cards[i].className = 'projectCard';
+        }
+        return;
+      }
+      ///////////////////////////////////
       entry.target.classList.add('show');
     }else{
       entry.target.classList.remove('show');
@@ -11,6 +18,23 @@ const observer = new IntersectionObserver((entries)=>{
 });
 
 const hiddenEles = document.querySelectorAll('.sections');
+const cards = document.querySelector('.gallery').children;
 
 
 hiddenEles.forEach((e)=> observer.observe(e));
+
+
+for (let i = 0; i < cards.length; i++) {
+  cards[i].addEventListener('mouseover', (e)=>{
+    resetGrids();
+    e.target.style.gridArea = 'content';
+  });
+}
+
+function resetGrids() {
+  let area = ['a', 'b', "c", "d", 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'content', 'content', 'content', 'content'];
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].style.gridArea = area[i];
+    cards[i].style.animation = 'showAnimation';
+  }
+}
