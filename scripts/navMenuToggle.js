8baa1e5cbc8a0b9aae6c2fb7b256ba51b1a1799f
ali houssa <81270840+itsalivenger@users.noreply.github.com>
2022-10-25 22:24:29 +0100

    e

--- scripts/navMenuToggle.js
+++ scripts/navMenuToggle.js
@@ -1,17 +1,8 @@
 let menu = document.querySelector('.toggle-Menu');
-let lightswitch = document.querySelector('.slider');
 let greg = false;
-let gregor = false;
 
 
 function toggle() {
     menu.className = greg ? 'toggle-Menu' : 'toggle-Menu toggle';
     greg = !greg;
 }
-
-function handleMode() {
-    gregor = !gregor;
-}
-
-
-lightswitch.onclick = handleMode;
